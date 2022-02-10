05929e9579342c61ba53d522b6b4b5f7d3eb7764
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-02-10 15:25:26 +0100

    Added some css to the profil page, the content doesn't grow tho... Still need to fix that issue when we will add pictures.

--- src/views/pages/profil.php
+++ src/views/pages/profil.php
@@ -16,27 +16,94 @@ if(isset($_COOKIE['user']) && !isset($_SESSION["login"])) {
 <?php include(__DIR__."/../partials/navBar.php"); ?>
 <main>
     <div class="profil">
-        <header class="profil-header">
-            <div class="profil-picture-container">
-                <div class="profil-picture">
-                    <img class="picture">
+        <div class="profil-container">
+            <header class="profil-header">
+                <div class="profil-picture-docker">
+                    <div class="profil-picture-container">
+                        <img class="profil-picture">
+                    </div>
                 </div>
-            </div>
-            <div class="profil-user">
-                <div class="profil-username-container">
-                    <span class="profil-username"></span>
+                <div class="profil-username-docker">
+                    <div class="profil-username-container">
+                        <span class="profil-username"></span>
+                    </div>
                 </div>
+            </header>
+
+            <div class="profil-gallery-separator">
+                <span>Create</span>
             </div>
-        </header>
 
-        <div class="profil-gallery-separator"></div>
-
-        <div class="profil-gallery-docker">
-            <article class="profil-gallery-container">
-                <!-- <div class="profil-gallery-content">
-                    <img class="gallery-content">
-                </div> -->
-            </article>
+            <div class="profil-gallery-docker">
+                <article class="profil-gallery-container">
+                    <!-- <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>
+                    <div class="profil-gallery-content">
+                        <img class="gallery-content">
+                    </div>     -->
+                </article>
+            </div>
         </div>
     </div>
 </main>
