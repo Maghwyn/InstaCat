20529395f0245db29df58441983945d9e4d534b6
KevinRAV <tsiorisoa-kevin.raveloarimanana@edu.esiee-it.fr>
2022-02-10 09:12:28 +0100

    Login and signin conditions

--- www/actions/registerManagement.php
+++ www/actions/registerManagement.php
@@ -5,37 +5,38 @@ function LogIn() {
 
     $are_fields_valid = false;
 
-    // $email = htmlspecialchars($_POST["the tag name"]);
-    // $password = htmlspecialchars($_POST["the tag name"]); // Don't hash the password.
+    $email = htmlspecialchars($_POST["log-in-user"]);
+    $password = htmlspecialchars($_POST["log-in-password"]);
 
     if(!empty($email) && !empty($password)) {
-        //$$$$$$$$$$$$$$$$$$$$$$ Just a template.
-        // include_once(__DIR__."BDD_infos_path -> config.php");
-        // include_once(__DIR__."CLASS_path -> database.php");
+        include_once(__DIR__."../src/config/config.php -> config.php");
+        include_once(__DIR__."../src/database.php -> database.php");
 
-        // try {
-        //     $bd = new BDD(config_db["host"], config_db["port"], config_db["dbname"], config_db["user"], config_db["pass"]);
-        //     $bd -> connect();
-        //     $dbtable = $bd -> select("SELECT * FROM User WHERE email = '$email'");
-        //     $user = $dbtable -> fetch(PDO::FETCH_ASSOC);
+        try {
+            $bd = new BDD(config_db["host"], config_db["port"], config_db["dbname"], config_db["user"], config_db["pass"]);
+            $bd -> connect();
+            $dbtable = $bd -> select("SELECT * FROM User WHERE email = '$email'");
+            $user = $dbtable -> fetch(PDO::FETCH_ASSOC);
 
-        //     if (password_verify($password, $user['password'])) $are_fields_valid = true;
-        // }
-        // catch (Exception $e) {
-        //     die($e->getMessage());
-        // }
+            if($email === $user['email'] && password_verify($password, $user['password'])) $are_fields_valid = true;
+        }
+        catch (Exception $e) {
+            die($e->getMessage());
+        }
     }
-
     //$$$$$$$$$$$$$$$$$$$$$$ When working on it, change the somewhere to the right page.
-    // if($are_fields_valid) {
-    //     header('Location: http://127.0.0.1:12001/www/index.php?p=somewhere');
-    //     session_destroy();
-    //     exit(); 
-    // }else {
-    //     header('Location: http://127.0.0.1:12001/www/index.php?p=register&error=code_error');
-    //     session_destroy();
-    //     exit();
-    // }
+    if($are_fields_valid) {
+        $_SESSION["email"] = $email;
+        $_SESSION["password"] = $password;
+        echo 'Login success';
+        header('Location: http://127.0.0.1:12001/www/index.php?p=somewhere');
+        session_destroy();
+        exit(); 
+    }else {
+        header('Location: http://127.0.0.1:12001/www/index.php?p=register&error=code_error');
+        session_destroy();
+        exit();
+    }
 }
 
 function SingIn() {
@@ -51,45 +52,54 @@ function SingIn() {
     shuffle($random_id);
 
     $key_id = intval(implode("", $random_id), 10);
-    // $pseudo = htmlspecialchars($_POST["the tag name"]);
-    // $email = htmlspecialchars($_POST["the tag name"]);
-    // $password = password_hash($_POST["the tag name"], PASSWORD_DEFAULT);
+    $pseudo = htmlspecialchars($_POST["sign-in-pseudo"]);
+    $email = htmlspecialchars($_POST["sign-in-email"]);
+    $password = password_hash($_POST["sign-in-password"], PASSWORD_DEFAULT);
+    $password_verified = password_hash($_POST["sign-in-password-verified"], PASSWORD_DEFAULT);
 
-    /********************
-        Verification of the field goes here :
-            - strlen($field) >= value    // Set the limit of characters in the field. 
-            - empty($field)              // Check if the field is empty.
-            - preg_match(regex, $field)  // Check if the field has an unwanted character, or doesn't follow a rule.
-
-        If one of these requirements is false, the form is invalid.
-    ********************/
-
-    if($are_fields_valid) {
-        //$$$$$$$$$$$$$$$$$$$$$$ Just a template.
-        // include_once(__DIR__."BDD_infos_path -> config.php");
-        // include_once(__DIR__."CLASS_path -> database.php");
-
-        // try {
-        //     $bd = new BDD(config_db["host"], config_db["port"], config_db["dbname"], config_db["user"], config_db["pass"]);
-        //     $user = new User($key_id, $pseudo, $email, $password);
+    if(empty($pseudo) || empty($email) || empty($password)){
+        $are_fields_valid = false;
+    }
+    else if(strlen($pseudo)<= 3 && strlen($password) <=3){
+        $are_fields_valid = false;
+    }else if($password_verified != $password) {
+        $are_fields_valid = false;
+    }
+    else{
+        if(preg_match(regex, $rgx_pseudo) && preg_match(regex, $email)&& preg_match(regex, $password)){
+            $are_fields_valid = true;
+        }
+        $are_fields_valid = true;
+    }
     
-        //     $bd -> connect();
-        //     $bd -> insert($user);
-        //     $bd -> disconnect();
-        // }
-        // catch (Exception $e) {
-        //     die($e->getMessage());
-        // }
+    if($are_fields_valid) {
+        include_once(__DIR__."../src/config/config.php -> config.php");
+        include_once(__DIR__."../src/database.php -> database.php");
 
+        try {
+            $bd = new BDD(config_db["host"], config_db["port"], config_db["dbname"], config_db["user"], config_db["pass"]);
+            $user = new User($key_id, $pseudo, $email, $password);
+    
+            $bd -> connect();
+            $bd -> insert($user);
+            $bd -> disconnect();
+            
+        }
+        catch (Exception $e) {
+            die($e->getMessage());
+        }
         //$$$$$$$$$$$$$$$$$$$$$$ When working on it, change the somewhere to the right page.
-        // header('Location: http://127.0.0.1:12001/www/index.php?p=somewhere');
-        // session_destroy();
-        // exit(); 
+        $_SESSION["pseudo"] = $pseudo;
+        $_SESSION["email"] = $email;
+        $_SESSION["password"] = $password;
+        $_SESSION["password_verified"] = $password_verified;
+        header('Location: http://127.0.0.1:12001/www/index.php?p=somewhere');
+        session_destroy();
+        exit(); 
     }else {
         //$$$$$$$$$$$$$$$$$$$$$$ Keep the values of the fields just in case, except the password.
-        // $_SESSION["pseudo"] = $pseudo;
-        // $_SESSION["email"] = $email;
-
+        $_SESSION["pseudo"] = $pseudo;
+        $_SESSION["email"] = $email;
         header('Location: http://127.0.0.1:12001/www/index.php?p=register&error=code_error');
         exit();
     }
