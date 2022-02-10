64728f99776667d319628bef788e6e9dc76b13af
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-02-10 18:52:20 +0100

    cookie update

--- src/views/pages/profil.php
+++ src/views/pages/profil.php
@@ -1,6 +1,20 @@
 <?php $page_title = 'profil'; /*$user_identifier = $_GET['user_identifier'];*/ ob_start(); ?>
-<?php include(__DIR__ . "/../partials/navBar.php"); ?>
+<?php 
+setcookie("user", "", time() - (86400 * 31), "/", false, false);
+var_dump($_COOKIE); //Debug
 
+if(isset($_COOKIE['user']) && !isset($_SESSION["login"])) {
+    // var_dump($_COOKIE['user']);
+    $user = $_COOKIE['user'];
+
+    $_SESSION["login"] = true;
+    $_SESSION["username"] = $user;
+}else {
+    header("Location: http://127.0.0.1:12001/www/index.php?p=register");
+}
+?>
+
+<?php include(__DIR__ . "/../partials/navBar.php"); ?>
 <main class="profil">
     <div class="profil-docker">
         <div class="profil-container">
