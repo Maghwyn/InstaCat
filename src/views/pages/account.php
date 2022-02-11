47b1a2f8680aa350ce4a302a183324071089a2e8
Maghwyn <christopher.samson@edu.esiee-it.fr>
2022-02-11 11:47:32 +0100

    Quickly designed the account page, we can change our password and our email.

--- src/views/pages/account.php
+++ src/views/pages/account.php
@@ -5,18 +5,24 @@
     <div class="parameter-container">
         <div class="parameter">
             <section class="parameter-menu">
-                <div class="parameter-username">
-                    <span>Username</span>
-                </div>
                 <div class="menu-account">
                     <span>Security</span>
                 </div>
             </section>
             <section class="parameter-option-docker">
                 <div class="parameter-option-container">
-                    <div class="option-password">
+                    <form class="option-password" action='/www/actions/accountManagement.php?id=changepass' method="POST">
                         <span>Change your password here</span>
-                    </div>
+                        <input name="old-pass" type="password" placeholder="Enter your old password"></input>
+                        <input name="new-pass" type="password" placeholder="Enter your new password"></input>
+                        <input type="submit"></input>
+                    </form>
+                    <form class="option-email" action='/www/actions/accountManagement.php?id=changeemail' method="POST">
+                        <span>Change your email here</span>
+                        <input name="current-pass" type="password" placeholder="Enter your old password"></input>
+                        <input name="new-email" type="email" placeholder="Enter your new email"></input>
+                        <input type="submit"></input>
+                    </form>
                 </div>
             </section>
         </div>
